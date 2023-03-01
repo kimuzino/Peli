3d04e27a1197f1bf4d0e5a0299486f815b781a88
kimuzino <113186426+kimuzino@users.noreply.github.com>
2023-03-01 07:29:03 +0000

    sinä jumaleuta

--- Taistelu.cpp
+++ Taistelu.cpp
@@ -170,123 +170,135 @@ void Taistelu1(){
             }
         }
 
-        clear_screen();
-        // Näyttää pelaajien HP
-        std::cout << "Pommittajan HP: " << pelaaja1 << "\n";
-        std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
+
+        while (true) {
+
+            clear_screen();
+            // Näyttää pelaajien HP
+            std::cout << "Pommittajan HP: " << pelaaja1 << "\n";
+            std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
 
 
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        // Aloitus teksti
-        std::cout << "Liikkeiden käyttö määrä\n" "Normaali liikkeet: " << n << "\n" "Erikoisliikkeet: " << x << "\n";
-        std::cout << "Valitse liike minkä käytäy\n" << "\n";
-        std::cout << "1. Nyrkki (10dmg)\n";
-        std::cout << "2. Kotitekoinen pommi (35dmg) (2N)\n";
-        std::cout << "3. (Ultimate) Ydinpommi (75dmg) (2N ja 1E)\n";
-        std::cout << "4. Erikoisliike: Adrenaliini piikki (+35hp) (2N)\n";
-        std::cout << "Valitse liike: ";
-        std::cin >> isku;
-
-        while (std::cin.fail()) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Valitse liike 1-4 väliltä!\n";
-            sleep_ms (1000);
+
+            // Aloitus teksti
+            std::cout << "Liikkeiden käyttö määrä\n" "Normaali liikkeet: " << n << "\n" "Erikoisliikkeet: " << x << "\n";
+            std::cout << "Valitse liike minkä käytäy\n" << "\n";
+            std::cout << "1. Nyrkki (10dmg)\n";
+            std::cout << "2. Kotitekoinen pommi (35dmg) (2N)\n";
+            std::cout << "3. (Ultimate) Ydinpommi (75dmg) (2N ja 1E)\n";
+            std::cout << "4. Erikoisliike: Adrenaliini piikki (+35hp) (2N)\n";
             std::cout << "Valitse liike: ";
             std::cin >> isku;
+
+            while (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Valitse liike 1-4 väliltä!\n";
+                sleep_ms (1000);
+                std::cout << "Valitse liike: ";
+                std::cin >> isku;
+                
+            }
+
+            // isku 1 koodi (Nyrkki)
+            if (isku == 1){
+                n++;
+                clear_screen();
+                pelaaja2 = pelaaja2 - 10;
+                std::cout << "\n" << "Pommittaja iski nyrkillä!\n";
+                sleep_ms (1000);
+                std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
+                sleep_ms (1000);
+                e++;
+                break;
+            }
+
+            // isku 2 koodi (Kootitekoinen pommi)
+            if (isku == 2 ) {
+
+                // isku 2 koodi (Kootitekoinen pommi) (Ei tarpeeksi liikkeitä)
+                if (isku == 2 && n < 2) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaalit pisteet : " << n << "\n";
+                    sleep_ms (2000);
+                    clear_screen();
+                }
+
+                if (n == 2 ||n >= 2){
+                    n--;
+                    clear_screen();
+                    pelaaja2 = pelaaja2 - 35;
+                    std::cout << "\n" << "Pommittaja iski kotitekoisella pommilla!\n";
+                    sleep_ms (1000);
+                    std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
+            }
+
+            // isku 3 koodi (Ydinpommi)
+            if (isku == 3) {
+
+                // isku 3 koodi (Ydinpommi) (Ei tarpeeksi liikkeitä)
+                if (isku == 3 && n < 2 || isku == 3 && x < 1 || isku == 3 && n < 2 && x < 1 ) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaali ja erikois pisteet : " << n << ", "<< x << "\n";
+                    sleep_ms (2000);
+                    clear_screen();
+                }
+
+                // isku 3 koodi (Ydinpommi) (Tarpeeksi liikkeitä)
+                if (n == 2 && x == 1|| isku == 3 && n >= 2 && x >= 1) {
+                    n--; n--; x--;
+                    clear_screen();
+                    pelaaja2 = pelaaja2 - 75;
+                    std::cout << "\n" << "Pommittaja iski ydinpommilla!\n";
+                    sleep_ms (1000);
+                    std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
+            }
+
             
+
+            // isku 4 koodi (Adrenaliini piikki)
+            if (isku == 4) {
+
+                // isku 4 koodi (Adrenaliini piikki) (Ei tarpeeksi liikkeitä)
+                if (n < 2) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaalit pisteet : " << n << "\n";
+                    sleep_ms (2000);
+                    clear_screen();
+                }
+
+                // isku 4 koodi (Adrenaliini piikki) (Tarpeeksi liikkeitä)
+                if (n == 2 || n >= 2 ) {
+                    x++; n--; n--;
+                    clear_screen();
+                    std::cout << "Pommittaja käytti erikoisliikkeen!\n";
+                    sleep_ms (1000);
+                    pelaaja1 = pelaaja1 + 30;
+                    std::cout << "Pommittajan HP: " << pelaaja1 << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
+
+                // isku 4 koodi (Adrenaliini piikki) (Liian iso HP taso)
+                //if (isku == 4 && pelaaja1 > 70) {
+                //    std::cout << "Liian iso HP määrä!\n";
+                //    std::cout << pelaaja1 << "\n";
+                //    sleep_ms (1500);
+                //}
+            }  
         }
 
-        // isku 1 koodi (Nyrkki)
-        if (isku == 1){
-            n++;
-            clear_screen();
-            pelaaja2 = pelaaja2 - 10;
-            std::cout << "\n" << "Pommittaja iski nyrkillä!\n";
-            sleep_ms (1000);
-            std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
-            sleep_ms (1000);
-            e++;
-        }
-
-        // isku 2 koodi (Kootitekoinen pommi)
-        if (isku == 2 ) {
-
-            // isku 2 koodi (Kootitekoinen pommi) (Ei tarpeeksi liikkeitä)
-            if (isku == 2 && n < 2) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaalit pisteet : " << n << "\n";
-                sleep_ms (1500);
-            }
-
-            if (n == 2 ||n >= 2){
-                n--;
-                clear_screen();
-                pelaaja2 = pelaaja2 - 35;
-                std::cout << "\n" << "Pommittaja iski kotitekoisella pommilla!\n";
-                sleep_ms (1000);
-                std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
-                sleep_ms (1000);
-                e++;
-            }
-        }
-
-        // isku 3 koodi (Ydinpommi)
-        if (isku == 3) {
-
-            // isku 3 koodi (Ydinpommi) (Ei tarpeeksi liikkeitä)
-            if (isku == 3 && n < 2 || isku == 3 && x < 1 || isku == 3 && n < 2 && x < 1 ) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaali ja erikois pisteet : " << n << ", "<< x << "\n";
-                sleep_ms (1500);
-            }
-
-            // isku 3 koodi (Ydinpommi) (Tarpeeksi liikkeitä)
-            if (n == 2 && x == 1|| isku == 3 && n >= 2 && x >= 1) {
-                n--; n--; x--;
-                clear_screen();
-                pelaaja2 = pelaaja2 - 75;
-                std::cout << "\n" << "Pommittaja iski ydinpommilla!\n";
-                sleep_ms (1000);
-                std::cout << "Alkoholistin HP: " << pelaaja2 << "\n" << "\n";
-                sleep_ms (1000);
-                e++;
-            }
-        }
-
-        
-
-        // isku 4 koodi (Adrenaliini piikki)
-        if (isku == 4) {
-
-            // isku 4 koodi (Adrenaliini piikki) (Ei tarpeeksi liikkeitä)
-            if (n < 2) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaalit pisteet" << n << "\n";
-                sleep_ms (1500);
-            }
-
-            // isku 4 koodi (Adrenaliini piikki) (Tarpeeksi liikkeitä)
-            if (n == 2 || n >= 2 ) {
-                x++; n--; n--;
-                clear_screen();
-                std::cout << "Pommittaja käytti erikoisliikkeen!\n";
-                sleep_ms (1000);
-                pelaaja1 = pelaaja1 + 30;
-                std::cout << "Pommittajan HP: " << pelaaja1 << "\n";
-                sleep_ms (1000);
-                e++;
-            }
-
-            // isku 4 koodi (Adrenaliini piikki) (Liian iso HP taso)
-            //if (isku == 4 && pelaaja1 > 70) {
-            //    std::cout << "Liian iso HP määrä!\n";
-            //    std::cout << pelaaja1 << "\n";
-            //    sleep_ms (1500);
-            //}
-        }  
 
         if (pelaaja2 <= 0) {
             clear_screen();
@@ -570,113 +582,122 @@ void Taistelu2(){
                 exit(0);
             }
         }
+        
 
-        clear_screen();
-        // Näyttää pelaajien HP
-        std::cout << "Alkoholistin HP: " << pelaaja1 << "\n";
-        std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
+        while (true) {
 
-        // Aloitus teksti
-        std::cout << "Liikkeiden käyttö määrä\n" "Normaali liikkeet: " << n << "\n" "Erikoisliikkeet: " << x << "\n";
-        std::cout << "Valitse liike minkä käytäy\n" << "\n";
-        std::cout << "1. Puukotus (20dmg)\n";
-        std::cout << "2. Pullon heitto (35dmg) (2N) \n";
-        std::cout << "3. (Ultimate) Isän käsi (50dmg) (2N ja 1E)\n";
-        std::cout << "4. Erikois liike. Kalja (+30hp) (2n)\n";
-        std::cout << "Valitse liike: ";
-        std::cin >> isku;
+            clear_screen();
+            // Näyttää pelaajien HP
+            std::cout << "Alkoholistin HP: " << pelaaja1 << "\n";
+            std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
 
-
-        while (std::cin.fail()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Valitse liike 1-4 väliltä!\n";
-            sleep_ms (1000);
+            // Aloitus teksti
+            std::cout << "Liikkeiden käyttö määrä\n" "Normaali liikkeet: " << n << "\n" "Erikoisliikkeet: " << x << "\n";
+            std::cout << "Valitse liike minkä käytäy\n" << "\n";
+            std::cout << "1. Puukotus (20dmg)\n";
+            std::cout << "2. Pullon heitto (35dmg) (2N) \n";
+            std::cout << "3. (Ultimate) Isän käsi (50dmg) (2N ja 1E)\n";
+            std::cout << "4. Erikois liike. Kalja (+30hp) (2n)\n";
             std::cout << "Valitse liike: ";
             std::cin >> isku;
-        }
 
 
-
-        // isku 1 koodi (Puukotus)
-        if (isku == 1){
-            n++;
-            clear_screen();
-            pelaaja2 = pelaaja2 - 20;
-            std::cout << "\n" << "Alkoholisti iski puukolla!\n";
-            sleep_ms (1000);
-            std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
-            sleep_ms (1000);
-            e++;
-        }
-
-        // isku 2 koodi (Pullon heitto)
-        if (isku == 2 ) {
-
-            // isku 2 koodi (Pullon heitto) (Ei tarpeeksi liikkeitä)
-            if (isku == 2 && n < 2) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaalit pisteet : " << n << "\n";
-                sleep_ms (1500);
+            while (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Valitse liike 1-4 väliltä!\n";
+                sleep_ms (1000);
+                std::cout << "Valitse liike: ";
+                std::cin >> isku;
             }
 
-            if (n == 2 ||n >= 2){
-                n--;
+
+
+            // isku 1 koodi (Puukotus)
+            if (isku == 1){
+                n++;
                 clear_screen();
-                pelaaja2 = pelaaja2 - 35;
-                std::cout << "\n" << "Alkoholisti heittää pullon!\n";
+                pelaaja2 = pelaaja2 - 20;
+                std::cout << "\n" << "Alkoholisti iski puukolla!\n";
                 sleep_ms (1000);
                 std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
                 sleep_ms (1000);
                 e++;
+                break;
+            }
+
+            // isku 2 koodi (Pullon heitto)
+            if (isku == 2 ) {
+
+                // isku 2 koodi (Pullon heitto) (Ei tarpeeksi liikkeitä)
+                if (isku == 2 && n < 2) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaalit pisteet : " << n << "\n";
+                    sleep_ms (2000);
+                }
+
+                if (n == 2 ||n >= 2){
+                    n--;
+                    clear_screen();
+                    pelaaja2 = pelaaja2 - 35;
+                    std::cout << "\n" << "Alkoholisti heittää pullon!\n";
+                    sleep_ms (1000);
+                    std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
+            }
+
+            // isku 3 koodi (Ydinpommi)
+            if (isku == 3) {
+
+                // isku 3 koodi (Ydinpommi) (Ei tarpeeksi liikkeitä)
+                if (isku == 3 && n < 2 || isku == 3 && x < 1 || isku == 3 && n < 2 && x < 1 ) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaali ja erikois pisteet : " << n << ", "<< x << "\n" << "\n";
+                    sleep_ms (2000);
+                }
+
+                // isku 3 koodi (Ydinpommi) (Tarpeeksi liikkeitä)
+                if (n == 2 && x == 1|| isku == 3 && n >= 2 && x >= 1) {
+                    n--; n--; x--;
+                    clear_screen();
+                    pelaaja2 = pelaaja2 - 50;
+                    std::cout << "\n" << "Alkoholisti käyttää isän kättä!\n";
+                    sleep_ms (1000);
+                    std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
+            }
+
+            // isku 4 koodi (Kalja)
+            if (isku == 4) {
+
+                // isku 4 koodi (Kalja) (Ei tarpeeksi liikkeitä)
+                if (n < 2) {
+                    std::cout << "Ei tarpeeksi Liikepisteitä!\n";
+                    std::cout << "Normaalit pisteet : " << n << "\n";
+                    sleep_ms (2000);
+                }
+
+                // isku 4 koodi (Kalja) (Tarpeeksi liikkeitä)
+                if (n == 2 || n >= 2 ) {
+                    x++; n--; n--;
+                    clear_screen();
+                    std::cout << "Alkoholisti juo kaljaa!\n";
+                    sleep_ms (1000);
+                    pelaaja1 = pelaaja1 + 30;
+                    std::cout << "Alkoholistin HP: " << pelaaja1 << "\n";
+                    sleep_ms (1000);
+                    e++;
+                    break;
+                }
             }
         }
 
-        // isku 3 koodi (Ydinpommi)
-        if (isku == 3) {
-
-            // isku 3 koodi (Ydinpommi) (Ei tarpeeksi liikkeitä)
-            if (isku == 3 && n < 2 || isku == 3 && x < 1 || isku == 3 && n < 2 && x < 1 ) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaali ja erikois pisteet : " << n << ", "<< x << "\n" << "\n";
-                sleep_ms (1500);
-            }
-
-            // isku 3 koodi (Ydinpommi) (Tarpeeksi liikkeitä)
-            if (n == 2 && x == 1|| isku == 3 && n >= 2 && x >= 1) {
-                n--; n--; x--;
-                clear_screen();
-                pelaaja2 = pelaaja2 - 50;
-                std::cout << "\n" << "Alkoholisti käyttää isän kättä!\n";
-                sleep_ms (1000);
-                std::cout << "Pommittajan HP: " << pelaaja2 << "\n" << "\n";
-                sleep_ms (1000);
-                e++;
-            }
-        }
-
-        // isku 4 koodi (Kalja)
-        if (isku == 4) {
-
-            // isku 4 koodi (Kalja) (Ei tarpeeksi liikkeitä)
-            if (n < 2) {
-                std::cout << "Ei tarpeeksi Liikepisteitä!\n";
-                std::cout << "Normaalit pisteet : " << n << "\n";
-                sleep_ms (1500);
-            }
-
-            // isku 4 koodi (Kalja) (Tarpeeksi liikkeitä)
-            if (n == 2 || n >= 2 ) {
-                x++; n--; n--;
-                clear_screen();
-                std::cout << "Alkoholisti juo kaljaa!\n";
-                sleep_ms (1000);
-                pelaaja1 = pelaaja1 + 30;
-                std::cout << "Alkoholistin HP: " << pelaaja1 << "\n";
-                sleep_ms (1000);
-                e++;
-            }
-        }
 
         if (pelaaja2 <= 0) {
             clear_screen();
